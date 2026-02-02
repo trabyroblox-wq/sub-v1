54efac3d15077dc0253aed19d0e2f76fdb417bfd
trabyroblox-wq <trabyroblox@gmail.com>
2026-02-02 15:16:20 -0500

    Create logger.js

---
+++ scr/logger.js
@@ -0,0 +1,11 @@
+import chalk from 'chalk';
+
+const prefix = chalk.bgBlue.black(' SUB v1 ');
+
+export const logger = {
+  info:    (msg) => console.log(prefix, chalk.cyan('info   '), msg),
+  ready:   (msg) => console.log(prefix, chalk.green('ready  '), msg),
+  request: (msg) => console.log(prefix, chalk.yellow('→      '), msg),
+  connect: (msg) => console.log(prefix, chalk.magenta('CONNECT'), msg),
+  error:   (msg) => console.error(prefix, chalk.red('ERROR  '), msg),
+};
